9caa1eef17cd2ffdb85ea1430b6d9be0958ee12d
Roseberry <165325276+sahar-km@users.noreply.github.com>
2024-05-02 06:29:25 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -9,7 +9,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = 'cf8cf683-40fa-4cd3-93cd-820071b11c90';
 
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
-const proxyIPs= ['138.3.254.39', '130.61.23.77', '45.142.115.59'];// OR USE ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
+const proxyIPs= ['138.3.254.39', '130.61.23.77'];// OR USE ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://dns.adguard-dns.com/dns-query';
